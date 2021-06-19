438bbe2ccc482ce7f395c683e1df9e326fd4a08d
Mirjam Block <mfblock@hotmail.com>
2021-06-19 10:58:14 +0200

    Opdracht 9
    1e Java script

---
+++ Winc_Academy/Opdracht 9 Start javascript/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let naam = "mirjam";
+console.log(naam);
+console.log(4+4);
+console.log("4" + "4");
+console.log(4 * 4);
+console.log(4/4);
+console.log(4 - 4);
+console.log( 100 % 9);
+let leeftijd = 49;
+console.log(typeof(leeftijd));
+let str_leeftijd = String(leeftijd);
+console.log(typeof(str_leeftijd));
