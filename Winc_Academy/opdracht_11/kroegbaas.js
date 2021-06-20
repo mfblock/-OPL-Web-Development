56ee4fa6b6f71774b565cfbe28abcdd55246400f
Mirjam Block <mfblock@hotmail.com>
2021-06-20 11:29:54 +0200

    kroegbaas opdracht
    waar om constanten en geen variable ??

---
+++ Winc_Academy/opdracht_11/kroegbaas.js
@@ -0,0 +1,19 @@
+const Age = 20
+const isFemale = new Boolean(true)
+const DriverStatus = 'bob'
+
+if (Age <= 18) {
+    console.log("Geen Bier voor jou")
+} else {console.log("Welkom wat wil je drinken ?")}
+
+if (isFemale) {
+    console.log("welkom veel plezier vannavond")
+} else {
+    console.log("hey Kerel vannavond niet, kom je morgen ?")
+}
+
+if (driverstatus = "bob") {
+    console.log("Ah bob the driver")
+} else {
+    console.log("sleutels inleveren jij mag niet rijden")
+}
